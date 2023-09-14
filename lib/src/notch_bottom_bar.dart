e9d1e9925094aaadb211c1df5d8a209152f60653
dodo07070707 <aa01056328583@gmail.com>
2023-09-14 16:19:05 +0900

    positioning!

--- lib/src/notch_bottom_bar.dart
+++ lib/src/notch_bottom_bar.dart
@@ -195,7 +195,7 @@ class _AnimatedNotchBottomBarState extends State<AnimatedNotchBottomBar>
                               top: widget.removeMargins
                                   ? -kCircleMargin / 2
                                   : kTopMargin - 4,
-                              left: (kCircleRadius - kCircleMargin + 32) / 2 +
+                              left: (kCircleRadius - kCircleMargin + 28) / 2 +
                                   _itemPosByScrollPosition(scrollPosition),
                               child: BottomBarActiveItem(
                                 i,
